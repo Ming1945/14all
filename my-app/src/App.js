441054a8b19aa0a1c833c90fcbe48c3a6a9c8dc3
Anthony Fernando <ming.fc45@yahoo.com>
2021-05-23 18:53:04 +0200

    14ALL title added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          coba-coba ae dulu
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Open Youtube
+          jangan diklik!
         </a>
       </header>
     </div>
